a4b229f6b97e19186b7bc1c08b56c23ad835ac8d
alex ferreira da silva <hatealex100@gmail.com>
2023-04-10 01:56:26 -0300

    project started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+
+      <div style={{marginTop: "-20px"}}>
+        
+        <div className="post">
+          <div className="image">
+            <img src="https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg" alt="" />
+          </div>
+          <div className="texts">
+            <h2>Full-house battery backup coming later this year</h2>
+            <p className="info">
+              <a className="author" href="">Dawid Paszko</a>
+              <time>2023-01-06 16:45</time>
+            </p>
+            <p className="summary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima doloribus libero perspiciatis animi dolor quasi.</p>
+          </div>
+        </div>
+        
+        <div className="post">
+          <div className="image">
+            <img src="https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg" alt="" />
+          </div>
+          <div className="texts">
+            <h2>Full-house battery backup coming later this year</h2>
+            <p className="info">
+              <a className="author" href="">Dawid Paszko</a>
+              <time>2023-01-06 16:45</time>
+            </p>
+            <p className="summary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima doloribus libero perspiciatis animi dolor quasi.</p>
+          </div>
+        </div>
+
+        <div className="post">
+          <div className="image">
+            <img src="https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg" alt="" />
+          </div>
+          <div className="texts">
+            <h2>Full-house battery backup coming later this year</h2>
+            <p className="info">
+              <a className="author" href="">Dawid Paszko</a>
+              <time>2023-01-06 16:45</time>
+            </p>
+            <p className="summary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima doloribus libero perspiciatis animi dolor quasi.</p>
+          </div>
+        </div>
+
+      </div>
+
+
+    </main>
   );
 }
 
