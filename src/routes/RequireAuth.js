c7358a2e872a339292f8876e93517b30c3ff78ca
alex ferreira da silva <hatealex100@gmail.com>
2023-04-22 00:00:34 -0300

    requireauth chnages

--- src/routes/RequireAuth.js
+++ src/routes/RequireAuth.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Navigate } from 'react-router-dom';
-import Cookies from "js-cookie";
+// import Cookies from "js-cookie";
+import { Context } from '../contexts/UserContext';
 
 
 export const RequireAuth = ({children, ...rest}) => {
-    
-    const isLogged = () => {
-        const token = Cookies.get("token");
-        return token ? true : false;
-    }
-    
-    const logged = isLogged();
-    console.log(logged);
+    // const isLogged = () => {
+    //     const token = Cookies.get("token");
+    //     return token ? true : false;
+    // }
+    // const logged = isLogged();
 
-    if(logged) {
+  const {state} = useContext(Context);
+
+
+    if(state.user.id) {
         return children;
     } else {
         <Navigate to={"/login"} />
